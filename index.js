d90f3959fba46d7626494e316b0ec7a0b97c7285
tritranduc11 <tritranduc11@gamil.com>
2021-05-25 19:16:04 +0700

    add view engine to server

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var express = require("express")
 var http = require('http');
+var path = require('path');
+var views_folder = path.join(__dirname,"./views")
 var app = express()
-var port = 8000
+var port = 80
 var server = http.createServer(app)
+console.log(views_folder)
+
+app.set("view engine","pug")
+app.set('views',views_folder);
 
 app.get("/",(req,res)=>{
-    res.send("ewhrh4uhgt")
+    res.render("index",{title : "home"})
 })
 
 server.listen(port,()=>{
